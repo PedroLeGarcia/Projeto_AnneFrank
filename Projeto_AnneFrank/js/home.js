e99f5d6dbd023f7661b29e123dbe5bf59df9842f
PedroLeGarcia <91338154+PedroLeGarcia@users.noreply.github.com>
2022-10-03 22:35:47 -0300

    Update home.js

--- Projeto_AnneFrank/js/home.js
+++ Projeto_AnneFrank/js/home.js
@@ -1,3 +1,8 @@
+ window.onload() = function(){
+ var url = 'https://virtuallibrary.profrodolfo.com.br/book.php';
+    var d = document.querySelector('.dados');
+     let btn = document.getElementById('buscar');
+    let campbusca = document.getElementById('txtBusca');
 //Funções do Menu
 function MenuShow() {
 	let menuMobile = document.querrySelector('.mobile-menu');
@@ -9,4 +14,35 @@ function MenuShow() {
 		document.querrySelector('.icon').src = "imagens/close_white_36dp.svg";	
 	}
 }
-//Livros
+//Login
+
+function PesquisarLivro(){
+        let search = document.getElementById('txtBusca').value.toUpperCase();
+        fetch(url)
+        .then(resposta => {
+            return resposta.json();
+        })
+        .then((json)=>{        
+            for(i = 0; i < json.length; i++){
+                if(json[i].titulo.toUpperCase().includes(search)){
+                    d.innerHTML+= `
+                        <div class="productBox">
+                            <img src="${json[i].capa}">
+                            <h3>${json[i].titulo}</h3>
+                            <h4>${json[i].ano}</h4>
+                        </div>
+                    `;
+                }
+            }
+        }).catch();
+    }
+
+
+    btn.addEventListener('click', function(){
+        d.innerHTML = ``;
+        PesquisarLivro();
+    });
+    ExibirLivros();
+}
+
+
